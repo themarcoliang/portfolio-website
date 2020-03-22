f36276d4dbf294097d8594dbd499ccca37ed83af
themarcoliang <marcoliang333@gmail.com>
2020-03-21 18:51:04 -0700

    first page creation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+      return (
+        <React.Fragment>
+          <div className='rows' style={{width:'100%'}}>
+            <div id='title' className='row'>
+              <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
+                <h1>Marco Liang</h1>
+              </ScrollAnimation>
+            </div>
+
+            <div id='subtitle' className='row'>
+              <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
+                <p>what am I<br/>who am i<br/>website building is<br/>hard as fuck</p>
+              </ScrollAnimation>
+            </div>
+          </div>
+        </React.Fragment>
+      );
+    }
 }
 
-export default App;
+export default App
