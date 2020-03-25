da78e14cdc043b6da8ff8f7c6679754bb50a5744
themarcoliang <marcoliang333@gmail.com>
2020-03-24 17:21:55 -0700

    Removed Scrollbar, Added Horizontal-Scroll

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Marco Liang`,
+    description: `Marco Liang Portfolio Site`,
+    author: `@Marco`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,18 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import "../styles/index.css"
 
-const IndexPage = () => (
+
+const IndexPage = () => (  
   <Fragment>
     <SEO title="Home" />
     <div id="page-container">
-      <div id="page" style={{backgroundColor: "blue"}}>
+      <div id="page1" className="page" style={{backgroundColor: "#133043"}}>
         <h1 style={{color:"white"}}>This is page 1</h1>
       </div>
-      <div id="page" style={{backgroundColor: "red"}}>
+      <div id="page2" className="page" style={{backgroundColor: "red"}}>
         <h1 style={{color:"white"}}>This is page 2</h1>
       </div>
-      <div id="page" style={{backgroundColor: "yellow"}}>
+      <div id="page3" className="page" style={{backgroundColor: "yellow"}}>
         <h1 style={{color:"black"}}>This is page 3</h1>
       </div>
     </div>
