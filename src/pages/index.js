c7de3e6750d55ad5d2c284645e6416984f58da60
themarcoliang <marcoliang333@gmail.com>
2020-03-27 00:55:30 -0700

    Subtitle + clock(placeholder)

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,11 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div id="page-container">
       <div id="page1" className="page" style={{backgroundColor: "#133043"}}>
-        <h1 id="title"style={{color:"white"}}>MARCO LIANG</h1>
+        <h4 id="clock">12:48 AM March 27, 2020</h4>
+        <h1 id="title">MARCO LIANG</h1>
+        <p id="subtitle">3/4th Year Computer Engineering Student<br/>
+           Simon Fraser University<br/>
+           Vancouver, Canada</p>
       </div>
       <div id="page2" className="page" style={{backgroundColor: "red"}}>
         <h1 style={{color:"white"}}>This is page 2</h1>
