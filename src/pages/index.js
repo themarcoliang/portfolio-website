39402d14cb6e4311f74ac8f8bc3fe6bd5e174901
themarcoliang <marcoliang333@gmail.com>
2020-04-22 03:26:26 -0700

    Updated Home Page
    Changed Background to Black

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+// import Layout from "../components/layout"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 import "../styles/index.css"
 
@@ -30,27 +30,28 @@ const IndexPage = () => (
     <div id="page-container">
       <div id="page1" className="page">
         <div id="left1">
-          <h4 id="clock">12:48 AM March 27, 2020</h4>
+          {/* <h4 id="clock">12:48 AM March 27, 2020</h4> */}
         </div>
         <div id="center1">
-          <h1 id="title">MARCO LIANG</h1>
+          <div id="container">
+            <img src="/stairs.png" alt="stairs" id="stairspic"/>
+            <h1 id="title">MARCO &emsp; <br/>&emsp;LIANG</h1>
+          </div>
         </div>
         <div id="right1">
-          <p id="subtitle">3/4th Year Computer Engineering Student<br/>
-           Simon Fraser University<br/>
-           Vancouver, Canada</p>
+          <p id="subtitle">welcome.</p>
         </div>
         
       </div>
       
       {/* <div style={{backgroundColor: "#133043", width: "20vw"}}/> */}
 
-      <div id="page2" className="page" style={{backgroundColor: "red"}}>
-        <h1 style={{color:"white"}}>This is page 2</h1>
+      <div id="page2" className="page">
+        <h1>This is page 2</h1>
       </div>
       
-      <div id="page3" className="page" style={{backgroundColor: "yellow"}}>
-        <h1 style={{color:"black"}}>This is page 3</h1>
+      <div id="page3" className="page">
+        <h1>This is page 3</h1>
       </div>
     </div>
 
