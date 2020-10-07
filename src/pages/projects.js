56588926386e4e9e6d48bf6a15c4bafc44594edf
themarcoliang <marcoliang333@gmail.com>
2020-10-06 22:49:45 -0700

    Page templates

---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header"
+import Navigation from "../components/navigation"
+import "../styles/styles.scss"
+
+export default function Projects() {
+  return (
+    <div>
+      <Header/>
+      <Navigation/>
+    </div>
+  )
+}
