760c3c2972040a3e18d0cb0d5e4792bd1e34c0d6
themarcoliang <marcoliang333@gmail.com>
2020-10-06 23:39:12 -0700

    Intro Paragrah

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,17 @@ import "../styles/styles.scss"
 
 export default function Home() {
   return (
-    <div>
+    <div className="intro">
       <Header/>
+      <p>
+        Hi! I'm Marco, a <mark class="red">Computer Engineering</mark> student at Simon Fraser <br/>
+          University, with a minor in Computing Science, and I'm expected  <br/>
+          to graduate in 2021. <br/>
+          <br/>
+          Currently, I'm working as an iOS Development intern at <mark class="red">Rivian</mark>, but <br/>
+          I'm always excited to learn about new opportunities, so feel free to <br/>
+          get in touch with me through any of the channels linked above! <br/>
+      </p>
       <Navigation/>
     </div>
   )
