c44e913ad498e72c0e15f6cde9f7573cbc57e92a
themarcoliang <marcoliang333@gmail.com>
2020-10-10 23:08:59 -0700

    Active button now working

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,15 +9,19 @@ export default class Projects extends React.Component {
     super();
     this.state = {
       description: data.joergen.description,
-      title: data.joergen.title
+      title: data.joergen.title,
+      active: "joergen"
     }
     // this.descriptionChange = this.descriptionChange.bind(this);
   }
-  descriptionChange(newDescription) {
+  updateDescription(newDescription) {
     this.setState({description: newDescription});
   }
-  titleChange(newTitle) {
+  updateTitle(newTitle) {
     this.setState({title: newTitle});
+  }
+  updateActive(newActive){
+    this.setState({active: newActive});
   }
 
   render(){
@@ -25,37 +29,42 @@ export default class Projects extends React.Component {
       <div id="projects">
         <Header/>
         <div className="list" id="project_list">
-            <button className="button active" onClick={() => {
-              this.descriptionChange(data.joergen.description)
-              this.titleChange("Joergen")}}>
+            <button className={`button ${this.state.active === "joergen" ? 'active' : ''}`} onClick={() => {
+              this.updateDescription(data.joergen.description)
+              this.updateTitle("Joergen")
+              this.updateActive("joergen")}}>
                 <span className="name">{data.joergen.title}</span>
                 <span className="date">{data.joergen.date}</span>
             </button>
   
-            <button className="button" onClick={() => {
-              this.descriptionChange(data.paper_plane.description)
-              this.titleChange("Paper Plane")}}>
+            <button className={`button ${this.state.active === "paper_plane" ? 'active' : ''}`} onClick={() => {
+              this.updateDescription(data.paper_plane.description)
+              this.updateTitle("Paper Plane")
+              this.updateActive("paper_plane")}}>
                 <span className="name">{data.paper_plane.title}</span>
                 <span className="date">{data.paper_plane.date}</span>
             </button>
   
-            <button className="button" onClick={() => {
-              this.descriptionChange(data.reversi_ai.description)
-              this.titleChange("Reversi-AI")}}>
+            <button className={`button ${this.state.active === "reversi_ai" ? 'active' : ''}`} onClick={() => {
+              this.updateDescription(data.reversi_ai.description)
+              this.updateTitle("Reversi-AI")
+              this.updateActive("reversi_ai")}}>
                 <span className="name">{data.reversi_ai.title}</span>
                 <span className="date">{data.reversi_ai.date}</span>
             </button>
             
-            <button className = "button" onClick={() => {
-              this.descriptionChange(data.risc_v_execunit.description)
-              this.titleChange("RISC-V ExecUnit")}}>
+            <button className = {`button ${this.state.active === "risc_v_execunit" ? 'active' : ''}`} onClick={() => {
+              this.updateDescription(data.risc_v_execunit.description)
+              this.updateTitle("RISC-V ExecUnit")
+              this.updateActive("risc_v_execunit")}}>
                 <span className="name">{data.risc_v_execunit.title}</span>
                 <span className="date">{data.risc_v_execunit.date}</span>
             </button>
   
-            <button className = "button" onClick={() => {
-              this.descriptionChange(data.s_talk.description)
-              this.titleChange("S-Talk")}}>
+            <button className = {`button ${this.state.active === "s_talk" ? 'active' : ''}`} onClick={() => {
+              this.updateDescription(data.s_talk.description)
+              this.updateTitle("S-Talk")
+              this.updateActive("s_talk")}}>
                 <span className="name">{data.s_talk.title}</span>
                 <span className="date">{data.s_talk.date}</span>
             </button>
@@ -70,14 +79,5 @@ export default class Projects extends React.Component {
       </div>
     )}
   
-      /* //  <p className = "project_description">
-      //     A Discord bot that streams YouTube music, can be controlled <br/>
-      //     through the iOS companion app. Built the bot itself using the  <br/>
-      //     <mark className="red">Discord.js</mark> API,  YouTube API, and written in JavaScript.  <br/>
-      //     <br/>
-      //     The iOS companion app is written in <mark className="red">Swift</mark>, implements  <br/>
-      //     <mark className="red">SiriKit</mark> for the  voice commands. The app and bot  <br/>
-      //     communicates through a <mark className="red">Websocket</mark> connection, and the bot <br/>
-      //     itself is hosted on a Raspberry pi.  <br/>
-      // </p> */
+    
 }
